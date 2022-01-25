a696e7592124f6e40487d7c94e4f3c2fe3b67b82
migno chrono <mignochrono.nc@gmail.com>
2022-01-25 13:36:21 -0500

    new shit added?

--- question_handler.py
+++ question_handler.py
@@ -4,7 +4,7 @@ while trigger==1:
     print('how many cats do you have?')
     numcats = input()
     try:
-        if int(numcats) >= 4:
+        if int(numcats) >= 5: #increased number of cats
             print ('thats is a lot of cats.')
             trigger=0
         else:
@@ -13,3 +13,4 @@ while trigger==1:
     except ValueError:
         print ('you did not enter a number')
         trigger = 1
+ #banana banana justa dding text to see how to commit changes LALALALA
