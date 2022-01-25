ef473d504dfa7397bfa2d02eb1eb28f1f83ece4a
migno chrono <mignochrono.nc@gmail.com>
2022-01-25 13:27:32 -0500

    commiting from terminal LOL

---
+++ question_handler.py
@@ -0,0 +1,15 @@
+#question handler test from course
+trigger = 1
+while trigger==1:
+    print('how many cats do you have?')
+    numcats = input()
+    try:
+        if int(numcats) >= 4:
+            print ('thats is a lot of cats.')
+            trigger=0
+        else:
+            print ('that is not  that many cats')
+            trigger=0
+    except ValueError:
+        print ('you did not enter a number')
+        trigger = 1
